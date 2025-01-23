49dd72d2a5af53c6028fa31177c69e435b4a61c7
Ishaan Singh <sing1125@purdue.edu>
2025-01-23 16:16:31 -0500

    update

--- pysrs/instruments/galvo_funcs.py
+++ pysrs/instruments/galvo_funcs.py
@@ -13,14 +13,9 @@ class Galvo:
             "extra_steps": 100,  # optional, for stability
             "offset_x": -1.2,  
             "offset_y": 1.5, 
-            "step_x": 0,  
-            "step_y": 0,  
             "dwell": 10,  # per (x,y) combo, in us
             "amp_x": 0.5, 
             "amp_y": 0.5,  
-            "freq_x": 100,  # ignored for raster
-            "freq_y": 1,  # ignored for raster
-            "duration": 5,  
             "rate": 10000,  # sampling rate
             "device": 'Dev1',  # nidaq device name
             "ao_chans": ['ao1', 'ao0']  # for galvos
@@ -39,15 +34,16 @@ class Galvo:
 
 
     def gen_raster(self):
-        self.dwell *= 1e-6 # convert to s from us
-        pixel_samples = max(1, int(self.dwell * self.rate))
-        total_samples = pixel_samples * self.numsteps_x * self.numsteps_y
+        self.dwell *= 1e-6  
+        pixel_samples = max(1, int(self.dwell * self.rate))  
+        total_rowsamples = pixel_samples * self.numsteps_x
+        total_samples = total_rowsamples * self.numsteps_y
 
-        x_waveform = np.linspace(-self.amp_x, self.amp_x, self.numsteps_x, endpoint=False)
-        x_waveform = np.repeat(x_waveform, pixel_samples)
+        x_row = np.linspace(-self.amp_x, self.amp_x, self.numsteps_x, endpoint=False)
+        x_waveform = np.tile(np.repeat(x_row, pixel_samples), self.numsteps_y)
 
-        y_steps = np.linspace(-self.amp_y, self.amp_y, self.numsteps_y)
-        y_waveform = np.repeat(y_steps, self.numsteps_x * pixel_samples)
+        y_steps = np.linspace(self.amp_y, -self.amp_y, self.numsteps_y)  
+        y_waveform = np.repeat(y_steps, total_rowsamples)
 
         if len(x_waveform) < total_samples:
             x_waveform = np.pad(x_waveform, (0, total_samples - len(x_waveform)), constant_values=x_waveform[-1])
@@ -85,23 +81,26 @@ if __name__ == '__main__':
         "ao_chans": ['ao1', 'ao0'],
         "amp_x": 0.5,
         "amp_y": 0.5,
-        "duration": 5,
-        "rate": 1e5,
+        "rate": 1e5, # hz
         "numsteps_x": 100,  
-        "numsteps_y": 100    # must be a integer divisor of numsteps_x for a true raster
+        "numsteps_y": 100 , # must be a integer divisor of numsteps_x for a true raster
+        "dwell": 10, # us
     }
 
     galvo = Galvo(config)
     galvo.waveform = galvo.gen_raster()
 
+    times = np.arange(galvo.waveform.shape[1]) / config['rate'] 
+
     plt.figure(figsize=(10, 6))
-    plt.plot(galvo.waveform[0], label='x, fast', color='black')
-    plt.plot(galvo.waveform[1], label='y, slow', color='blue')
+    plt.plot(times, galvo.waveform[0], label='x, fast', color='black')
+    plt.plot(times, galvo.waveform[1], label='y, slow', color='blue')
     plt.legend()
-    plt.xlabel('Time, s')
-    plt.ylabel('Voltage, V')
-    plt.title('raster scan waveforms')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Voltage (V)')
+    plt.title('Raster Scan Waveforms with Time Axis')
     plt.grid()
+    plt.tight_layout()
     plt.show()
 
    #  galvo.do_raster()
